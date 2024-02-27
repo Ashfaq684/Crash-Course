e68c3281ffcc292380788f05b3ebec3a4a84ae3d
Ashfaq Ahmed Khan <ashfaq.mostahid@gmail.com>
2024-02-27 21:41:32 +0600

    crash course

---
+++ tailwind/tailwind-tales/src/App.jsx
@@ -0,0 +1,77 @@
+
+function App() {
+
+  return (
+    <>
+      {/* <div className="text-3xl font-bold underline">
+        <h1>Hello World!</h1>
+      </div>
+
+      <div>
+        <p className="text-red-500 text-xl text-left">Tom</p>
+        <p className="text-gray-400 text-2xl text-center">Jerry</p>
+        <p className="text-blue-900 text-3xl text-right">Max</p>
+      </div>
+
+      <div className="bg-red-500 bg-opacity-50 min-w-40 min-h-60">
+        TailwindCSS is great
+      </div>
+      <div className="bg-blue-200 w-1/2 h-32">
+        TailwindCSS is awesome
+      </div>
+      <div className="bg-gray-900 text-white m-4 p-8">
+        TailwindCSS is fantastic
+      </div>
+
+      <div className="bg-[url('https://nypost.com/wp-content/uploads/sites/2/2016/03/6o5a0355-1.jpg?quality=75&strip=all')] bg-center bg-no-repeat bg-cover size-96">
+      </div>
+
+      <div className="bg-gradient-to-r from-blue-300 via-purple-500 to-pink-300">
+        We are learning gradient
+      </div> */}
+
+      {/* <div className="flex">
+        <div className="flex-1 border border-green-800">First div 1</div>
+        <div className="flex-2 border border-pink-700">Second div 2</div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <div>
+          First
+        </div>
+        <div>
+          Second
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-2">
+          Grid Item 1
+        </div>
+        <div>
+          Grid Item 2
+        </div>
+      </div> */}
+
+      {/* <div className="bg-blue-300 sm:bg-red-300 md:bg-green-300 lg:bg-yellow-300 xl:bg-purple-500">
+        We are testing responsiveness!!!
+      </div>
+
+      <div className="bg-primary text-customcolor-900 font-custom">
+        I am testing custom colors
+      </div>
+
+      <p className="mt-2 ml-1 tablet:flex">Test</p> */}
+
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8">
+        <div>
+          <span className="inline-flex items-center justify-center p-2 bg-blue-500">
+            Some image
+          </span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
